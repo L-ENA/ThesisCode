dcdb6a8781037a4d62d86bebf7fe9f496b65c8a1
L-ENA <lena.schmidt@hs-furtwangen.de>
2018-03-02 18:21:18 +0000

    Started code for traversing and filling variables with vitamine e review

--- thesis/src/TrialObject.java
+++ thesis/src/TrialObject.java
@@ -1,11 +1,15 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class TrialObject {
 	
-	protected String author;
-	protected String datePublished;
+	protected String author; //check
+	protected int year;//check
 	protected String doi;
-	protected int trialID;
-	protected String revManID;
+	protected int crgID;
+	protected String revManID;//check
 	//protected RANDOMIZATIONTYPE randomisation:
 	//protected ALLOCATIONTYPE allocation;
 	//protected BLINDINGTYPE blinding;
@@ -19,6 +23,42 @@ public class TrialObject {
 	protected int nrControl;
 	//protected SETTING setting;
 	//protected outcomeList : OutcomeObjects
+	
+	public TrialObject(Document review, int studyNumber){
+					
+		//Traverses XML until the desired study is found
+					NodeList rootList = review.getElementsByTagName("COCHRANE_REVIEW");
+					Node rootNode = rootList.item(0);
+					Element rootElement = (Element) rootNode;
+					
+					NodeList characteristicsOfStudiesList = rootElement.getElementsByTagName("CHARACTERISTICS_OF_STUDIES");
+					Node characteristicsOfStudiesNode = characteristicsOfStudiesList.item(0);
+					Element characteristicsOfStudiesElement = (Element) characteristicsOfStudiesNode;
+					
+					NodeList characteristicsOfIncludedStudiesList = characteristicsOfStudiesElement.getElementsByTagName("CHARACTERISTICS_OF_INCLUDED_STUDIES");
+					Node characteristicsOfIncludedStudiesNode = characteristicsOfIncludedStudiesList.item(0);
+					Element characteristicsOfIncludedStudiesElement = (Element) characteristicsOfIncludedStudiesNode;
+					
+					NodeList includedStudiesList = characteristicsOfIncludedStudiesElement.getElementsByTagName("INCLUDED_CHAR");
+					Node studyToExtractNode = includedStudiesList.item(studyNumber);
+					Element studyToExtractElement = (Element) studyToExtractNode;
+					//Extracts information into variables
+					revManID = studyToExtractElement.getAttribute("STUDY_ID"); //Gets revman ID for study
+					String cache = revManID.replaceAll("[^\\d.]", "");
+					year = Integer.parseInt(cache); //Takes year of publication from ID
+					cache = revManID.replaceAll("STD-", "");
+					String[] cacheArray = cache.split("-");
+					author = cacheArray[0]; //Taken name of author from ID
+							
+					
+					System.out.println(revManID);
+					System.out.println(year);
+					System.out.println(author);
+					
+					
+					
+					
+	}
 	
 
 
