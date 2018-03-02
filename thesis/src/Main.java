c0c4c5800397eb33c081b2cf5bf4b21e9a9b3a54
L-ENA <lena.schmidt@hs-furtwangen.de>
2018-03-02 18:22:59 +0000

    added for loop and int to describe number of included studies

--- thesis/src/Main.java
+++ thesis/src/Main.java
@@ -1,14 +1,26 @@
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 
 public class Main {
 
 	
 	public static void main(String[] args) throws ParserConfigurationException {
-		ParseReview parserObject = new ParseReview();
-		Document[] reviewArray = parserObject.chooseAndParse(); //contains reviews specified by user
+		ParseReview parser = new ParseReview();
+		Document review = parser.chooseAndParse(); //contains reviews specified by user
+		int numberOfStudies = parser.numberOfIncludedStudies(review);
+		
+	for (int i = 0; i < numberOfStudies; i++){
+			
+			TrialObject a = new TrialObject(review, i);
+			
+			
+			
+		}
 		
 
 	}
