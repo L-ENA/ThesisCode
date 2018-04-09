a18004b12b378f86eea50e7e175fde7914760df6
L-ENA <lena.schmidt@hs-furtwangen.de>
2018-04-09 19:57:45 +0100

    JAXBContext now knows more classes

--- thesis/src/core/Main.java
+++ thesis/src/core/Main.java
@@ -1,9 +1,19 @@
 package core;
 import java.io.File;
+import java.io.StringWriter;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 //test2
 
@@ -11,17 +21,40 @@ public class Main {
 	
 
 	
-	public static void main(String[] args)  throws ParserConfigurationException{
+	public static void main(String[] args)  throws Exception{
 		
 		
 		Database d = new Database();//in Database class a list of trials in this review will be created
 		d.makeList();
 		
 	try {
-		JAXBContext jaxbContext = JAXBContext.newInstance(Database.class);
+		JAXBContext jaxbContext = JAXBContext.newInstance(
+				
+				Database.class,
+				
+				references.JournalReferenceObject.class,
+				references.BookReferenceObject.class,
+				references.BookSectionReferenceObject.class,
+				references.OtherReferenceObject.class,
+				references.ConferenceReferenceObject.class,
+				references.CorrespondenceReferenceObject.class,
+				references.CochraneProtocolReferenceObject.class,
+				references.CochraneReviewReferenceObject.class,
+				references.SoftwareReferenceObject.class,
+				references.UnpublishedReferenceObject.class,
+				
+				trialAndOutcome.ContinuousOutcomeObject.class,
+				trialAndOutcome.DichotomousOutcomeObject.class
+				
+				
+				
+				);
 		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		jaxbMarshaller.marshal(d, new File("vitE.xml"));	//puts database into a xml file
+		String path = "vitE.xml";
+		jaxbMarshaller.marshal(d, new File(path));	//puts database into a xml file that is saved according to path String
+		
+		
 		
 		
 	} catch (JAXBException e) {
@@ -31,5 +64,8 @@ public class Main {
 		
 
 	}
-
+	
+	
+	
+	
 }
