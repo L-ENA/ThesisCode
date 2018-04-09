79a032272fd4b9583112e5dda562222cfc580547
L-ENA <lena.schmidt@hs-furtwangen.de>
2018-04-09 19:44:32 +0100

    Setting is an enum that can take values to describe inpatient, outpatient and other settings.

--- thesis/src/trialAndOutcome/SETTING.java
+++ thesis/src/trialAndOutcome/SETTING.java
@@ -2,7 +2,7 @@ package trialAndOutcome;
 
 public enum SETTING {
 	
-	INPATIENT("inpatients"), OUTPATIENT("outpatients"), INANDOUT("inpatients and outpatients"), EMERGENCY("emergency room setting"), COMMUNITY("community setting"), HOSPITAL("hospital setting"), NOTAVAILABLE("setting not extracted");
+	INPATIENT("inpatient setting"), OUTPATIENT("outpatient setting"), INANDOUT("inpatient and outpatient setting"), EMERGENCY("emergency room setting"), COMMUNITY("community setting"), HOSPITAL("hospital setting"), NOTAVAILABLE("setting not extracted");
 	
 	private final String description;
 
